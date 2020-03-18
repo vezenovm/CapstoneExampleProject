98d1b6381f0d7580aa801b8fd3e6a62af1dab5cf
Maxim Vezenov <mvezenov@gmail.com>
2020-03-18 00:19:30 -0400

    fixed ExampleToken contract tests

--- contracts/ExampleToken.sol
+++ contracts/ExampleToken.sol
@@ -5,19 +5,18 @@ import "@openzeppelin/contracts/math/SafeMath.sol";
 
 contract ExampleToken is ERC20 {
 	using SafeMath for uint256;
+	mapping (address => uint) balances;
 
 	string public name = "ExampleToken";
 	string public symbol = "ET";
 	uint8 public decimals = 2;
-	uint256 public INITAL_SUPPLY = 420;
-	ERC20 public token;
-	address payable public wallet;
 	uint256 public rate;
-	uint256 public weiRaised;
+	address public owner;
 
 	constructor() public {
 		rate = 10;
-		wallet = msg.sender;
+		owner = msg.sender;
+		balances[owner] = 10000;
 	}
 
 	function stabilizeRate() public {
@@ -28,18 +27,38 @@ contract ExampleToken is ERC20 {
 		_transfer(from, to, value);
 	}
 
-	function buyExampleToken(address _beneficiary) public payable {
-		require(_beneficiary != address(0));
+	function sendCoin(address receiver, uint amount) public returns(bool sufficient) {
+        if (balances[msg.sender] < amount) return false;
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+        _transfer(msg.sender, receiver, amount);
+        return true;
+    }
+
+	function buyExampleToken() public payable {
 		require(msg.value != 0);
 
-		uint256 tokensToETC = msg.value.mul(rate);
-		weiRaised = weiRaised.add(msg.value);
+		// uint256 tokensToETC = msg.value.mul(rate);
+		// weiRaised = weiRaised.add(msg.value);
 
-		_mint(_beneficiary, tokensToETC);
-		token.transfer(_beneficiary, tokensToETC);
-		wallet.transfer(msg.value);
+		// _mint(_beneficiary, tokensToETC);
+		// token.transfer(_beneficiary, tokensToETC);
+		// wallet.transfer(msg.value);
+
+
+		uint256 numTokens = msg.value.mul(rate);
+		uint256 nextBalance = address(this).balance.add(msg.value);
+
+		require(nextBalance > address(this).balance);
+		_mint(msg.sender, numTokens);
+		//owner.transfer(msg.value);
+		_transfer(msg.sender, owner, msg.value);
 
 		stabilizeRate();
+	}
+
+	function getExTokenBalance(address addr) public view returns(uint) {
+		return balances[addr];
 	}
 
 }
--- test/ExampleToken.test.ts
+++ test/ExampleToken.test.ts
@@ -6,52 +6,158 @@ const json = require("./../build/contracts/ExampleToken.json");
 
 let accounts;
 let exampleToken;
-let manager;
+let owner;
 const interface = json["abi"];
 const bytecode = json["bytecode"];
 
-beforeEach(async () => {
-  accounts = await web3.eth.getAccounts();
-  manager = accounts[0];
-  exampleToken = await new web3.eth.Contract(interface)
-    .deploy({ data: bytecode })
-    .send({ from: manager, gas: "4000000" });
+const ExampleToken = artifacts.require("ExampleToken");
+var deployed;
+
+contract("ExampleToken", accounts => {
+  it("should put 10000 ExampleToken in the first account", () =>
+    ExampleToken.deployed()
+      .then(function(instance) {
+        deployed = instance;
+        return deployed.getExTokenBalance(accounts[0]);
+      })
+      .then(function(balance) {
+        assert.equal(
+          balance.valueOf(),
+          10000,
+          "10000 wasn't in the deploying account"
+        );
+      }));
+
+  it("can exchange token for ETC", () => {
+    let meta;
+
+    // Get intial balances of first and second account
+    const accountOne = accounts[0];
+    const accountTwo = accounts[1];
+
+    let accountOneStartingBalance;
+    let accountTwoStartingBalance;
+    let accountOneEndingBalance;
+    let accountTwoEndingBalance;
+
+    let accountOneStartingEthBalance;
+    let accountTwoStartingEthBalance;
+    let accountOneEndingEthBalance;
+    let accountTwoEndingEthBalance;
+
+    let startingRate;
+    let endingRate;
+
+    const amount = 10;
+
+    ExampleToken.deployed()
+      .then(function(instance) {
+        meta = instance;
+        return meta.getBalance.call(accountOne);
+      })
+      .then(function(balance) {
+        accountOneStartingBalance = balance.toNumber();
+        return meta.getBalance.call(accountTwo);
+      })
+      .then(function(balance) {
+        accountTwoStartingBalance = balance.toNumber();
+        return meta.rate.call();
+      })
+      .then(function(rate) {
+        startingRate = rate.toNumber();
+        return web3.eth.getBalance(accountOne);
+      })
+      .then(function(ethBalance) {
+        accountOneStartingEthBalance = ethBalance;
+        return meta.buyExampleToken({
+          from: accountTwo,
+          value: web3.utils.toWei("3", "ether")
+        });
+      })
+      .then(() => deployed.getBalance.call(accountOne))
+      .then(function(balance) {
+        accountOneEndingBalance = balance.toNumber();
+        return meta.getBalance.call(accountTwo);
+      })
+      .then(function(balance) {
+        accountTwoEndingBalance = balance.toNumber();
+        return web3.eth.getBalance(accountOne);
+      })
+      .then(function(ethBalance) {
+        accountOneEndingEthBalance = ethBalance;
+        return meta.rate.call();
+      })
+      .then(function(rate) {
+        endingRate = rate.toNumber();
+
+        assert.equal(
+          accountTwoEndingBalance,
+          accountTwoStartingBalance + startingRate,
+          "ExampleToken amount was correctly sent to the receiver"
+        );
+
+        assert.equal(
+          accountOneEndingEthBalance,
+          accountTwoStartingEthBalance + web3.utils.toWei("3", "ether"),
+          "Correct amount of ether was transferred to the contract owner"
+        );
+      });
+  });
 });
 
-describe("ExampleToken", () => {
-  it("deploys a contract", async () => {
-    const exampleTokenManager = await exampleToken.methods.wallet.call();
-    assert.equal(
-      manager,
-      exampleTokenManager,
-      "The manager is the one who launches the smart contract."
-    );
-  });
+// beforeEach(async () => {
+//   accounts = await web3.eth.getAccounts();
+//   console.log(accounts);
+//   owner = accounts[0];
+//   exampleToken = await new web3.eth.Contract(interface)
+//     .deploy({ data: bytecode })
+//     .send({ from: owner, gas: "4000000" });
+// });
 
-  it("can exchange token for ETC", async () => {
-    buyer = accounts[1];
+// describe("ExampleToken", () => {
+//   it("deploys a contract", async () => {
+//     const exampleTokenOwner = exampleToken.methods.owner.call();
+//     console.log("Owner: " + owner);
+//     console.log("Example Token Owner From Call: " + exampleTokenOwner);
+//     assert.equal(
+//       owner,
+//       exampleTokenOwner,
+//       "The owner is the one who launches the smart contract."
+//     );
+//   });
 
-    startingRate = await exampleToken.methods.rate.call();
+//   it("can exchange token for ETC", async () => {
+//     buyer = accounts[1];
 
-    await exampleToken.methods
-      .buyExampleToken(buyer)
-      .send({ value: web3.utils.toWei("3", "ether") });
+//     startingRate = await exampleToken.methods.rate.call();
 
-    weiRaisedByContract = await exampleToken.methods.weiRaised.call();
+//     startingContractBalance = await web3.eth.getBalance(owner);
 
-    assert.equal(
-      weiRaisedByContract,
-      web3.utils.toWei("3", "ether"),
-      "The correct amount is transferred to the wallet manager."
-    );
+//     assert.equal(
+//     	startingContractBalance,
+//     	web3.utils.toWei("0", "ether"),
+//     	"The contract owner balance starts at 0/"
+//     );
 
-    endingRate = await exampleToken.methods.rate.call();
-    endingSupply = await exampleToken.methods.totalSupply().call();
+//     await exampleToken.methods
+//       .buyExampleToken()
+//       .send({ from: buyer, value: web3.utils.toWei("3", "ether") });
 
-    assert.equal(
-      endingRate,
-      startingRate * endingSupply,
-      "The new exchange rate for the example token is correctly reflected."
-    );
-  });
-});
+//     endingContractBalance = await web3.eth.getBalance(owner);
+
+//     assert.equal(
+//       endingContractBalance,
+//       web3.utils.toWei("3", "ether"),
+//       "The correct amount is transferred to the contract owner."
+//     );
+
+//     endingRate = await exampleToken.methods.rate.call();
+//     endingSupply = await exampleToken.methods.totalSupply().call();
+
+//     assert.equal(
+//       endingRate,
+//       startingRate * endingSupply,
+//       "The new exchange rate for the example token is correctly reflected."
+//     );
+//   });
+// });
